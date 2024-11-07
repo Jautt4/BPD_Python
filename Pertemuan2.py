87db133f5b342e1f6d41ff1ef050f419f1c13b90
Ferdi Hariansyah <hariansyahsada987@gmail.com>
2024-11-07 18:45:08 +0700

    Add files via upload

---
+++ Pertemuan2.py
@@ -0,0 +1,27 @@
+nilaitk=[90, 75, 95, 85, 80]
+nilaiku= (nilaitk[2])
+nama="Ferdi Hariansyah"
+umur= 19
+alamat="Ciledug"
+
+print("="*50)
+print("Program List")
+print("NilaiTK = ",nilaitk)
+print("="*50)
+print("Mencetak Semua isi List : ")
+print(nilaitk)
+print("="*50)
+print("Mencetak isi list index ke-1")
+print(nilaitk[1])
+print("="*50)
+print("Mencetak isi list mulai dari index ke-1 sampai index ke-2 ")
+print(nilaitk[1:3])
+print("="*50)
+print("mengambil isi list index ke-2 ke variabel nilaiku")
+print("nilaiKu = ", nilaiku)
+print("="*50)
+print("\n")
+
+print("Nama saya adalah = ",nama)
+print("umur saya adalah = ",umur)
+print("Alamat saya di   = ",alamat)
