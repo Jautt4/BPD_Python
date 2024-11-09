e302dd8721b5aa7577d10109fa8503a2d249df1b
Ferdi Hariansyah <hariansyahsada987@gmail.com>
2024-11-09 22:12:51 +0700

    Add files via upload

---
+++ Pertemuan4L1.py
@@ -0,0 +1,4 @@
+print("Isi my_friends indeks ke-2 adalah: Agung")
+print("Isi my_friends indeks ke-4 adalah: Adam")
+print("Semua temen berjumlah: 5")
+print("Semua temen itu adalah : Anggun, Dian, Agung, Adi, Adam")
