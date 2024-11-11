7cd1e58db1c60da9641091ffa8fff76501455480
Ferdi Hariansyah <hariansyahsada987@gmail.com>
2024-11-11 17:22:00 +0700

    Add files via upload

---
+++ Pertemuan4L3.py
@@ -0,0 +1,11 @@
+nama="Rudi Hartono"
+umur="22"
+email="rudi.hartono@gmail.com"
+hobi=("koding, membaca, travelling, kuliner")
+menikah="Belum"
+twitter="@RudiH"
+
+print("Nama saya adalah ",nama)
+print("Email saya adalah ",email)
+print("Hobi saya adalah ",hobi)
+print("Twitter: ",twitter)
